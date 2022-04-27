713c5a750c2d31012f76efd974cf5934498f7792
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-04-27 15:38:23 -0500

    adding rows to table with htmx from hardcoded list

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Request, Header
+from fastapi.responses import HTMLResponse
+# from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/')
-def home():
-    return "hi"
+@app.get('/index/', response_class=HTMLResponse)
+def index(request: Request, hx_request: Optional[str] = Header(None)):
+    films = [
+        {'name': 'Blade Runner', 'director': 'Ridley Scott'},
+        {'name': 'Pulp Fiction', 'director': 'Quentin Tarantino'},
+        {'name': 'Mulholland Drive', 'director': 'David Lynch'},
+    ]
+    context = {'request': request, "films": films}
+    
+    if hx_request:
+        return templates.TemplateResponse("partials/table.html", context)
+    return templates.TemplateResponse("index.html", context)
+
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
